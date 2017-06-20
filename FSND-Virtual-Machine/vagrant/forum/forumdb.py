e8775b3b68ac3a1384d793583f515939ccfb61ee
Air Walk <air-walk@users.noreply.github.com>
2017-06-19 20:44:14 -0400

    connecting front-end to the forum DB

--- FSND-Virtual-Machine/vagrant/forum/forumdb.py
+++ FSND-Virtual-Machine/vagrant/forum/forumdb.py
@@ -1,15 +1,27 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
+def get_connection():
+  return psycopg2.connect("dbname=forum")
+
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  conn = get_connection()
+  cur  = conn.cursor()
+  cur.execute("SELECT content, time from posts order by time desc")
+  posts = cur.fetchall()
+  conn.close()
+
+  return posts
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  conn = get_connection()
+  cur  = conn.cursor()
+  cur.execute("INSERT INTO posts (content, time) VALUES (%s, %s)", (content, datetime.datetime.now()))
+  conn.commit()
+  conn.close()
